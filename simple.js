984e14e607475aee4ddde0602d4b7c0c95915633
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2018-09-05 14:46:42 -0500

    done

--- simple.js
+++ simple.js
@@ -2,17 +2,19 @@
 let bankAccount = 100000;
 const rent = 1200;
 const carPayment = 250;
-
+let onemonth = bankAccount - rent - carPayment;
+console.log(onemonth);
 //Guess what this will do!
 bankAccount = bankAccount - 12*rent;
 console.log(bankAccount);
-
 //Did you get what you expect?
 //Write a new statement below which subtracts 5 years of a car payment on line 12.
-
+bankAccount = bankAccount - 60*carPayment
 
 //Declare a variable named income on line 15, assign yourself 2500 in income.
+let income = 2500
 
 //On the next line
+
 
 
--- vars.js
+++ vars.js
@@ -1,8 +1,8 @@
 let a = 10;
 let b = -25;
 //create two more variables c and d and give them whatever number you want.
-
-
+let c = -10;
+let d = 25;
 const total = a+b+c+d;
 console.log(total);
 
@@ -12,13 +12,13 @@ console.log(total);
 //new section on string variables
 let convo = "hello ";
 //set name to your name, first and last.
-let name;
+let name = "Jeffrey Schultz";
 //add string convo and name together.
-const greeting =
+const greeting = convo + name;
 console.log(greeting);
 
 //make your own message similar to what is above.
-let convo2;
-let
-const
-console.log();
+let convo2 = "greetings "; 
+let convo3 = "human "; 
+const open = convo2 + convo3; 
+console.log(open);
